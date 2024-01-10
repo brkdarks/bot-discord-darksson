7f8991039312793d0c796e62f6a5553014fc61d8
Darks <ueliton.jr.abreu@gmail.com>
2024-01-10 00:36:12 -0300

    Tribunal fix final

--- commands/utility/tribunal.js
+++ commands/utility/tribunal.js
@@ -6,17 +6,16 @@ module.exports = {
 		.setDescription("Tornar o usuário réu.")
 		.addUserOption((option) => option.setName("target").setDescription("O usuário selecionado").setRequired(true)),
 	async execute(interaction) {
-		if (!interaction.member.roles.cache.some((role) => role.permissions.has("ADMINISTRATOR"))) {
-			await interaction.reply(`Você não tem permissão para usar esse comando!`);
-		} else {
+		if (interaction.member.roles.cache.some((role) => role.permissions.has("ADMINISTRATOR"))) {
 			const target = interaction.options.getUser("target");
 			const member = interaction.guild.members.cache.get(target.id);
 			const roleName = "Réu";
 			const role = interaction.guild.roles.cache.find((role) => role.name === roleName);
 
 			await member.roles.set([role]);
-
 			await interaction.reply(`${member} foi declarado réu!`);
+		} else {
+			await interaction.reply(`Você não tem permissão para usar esse comando!`);
 		}
 	},
 };
